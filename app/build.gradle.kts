5316644d43b394fa6fc444be1cbdefb163a13d60
bekir <coniconilesh@gmail.com>
2026-01-06 22:57:45 +0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 35
         targetSdk = 36
         versionCode = 10
-        versionName = "0.9.2"
+        versionName = "0.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
